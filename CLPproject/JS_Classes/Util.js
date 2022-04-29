1623223b6a0f5d56c69d3ca5b3596be4de7865b9
acelaena <62402838+acelaena@users.noreply.github.com>
2022-04-29 16:10:07 -0700

    aaaa

---
+++ CLPproject/JS_Classes/Util.js
@@ -0,0 +1,19 @@
+function rand(min, max) {
+  return Math.round(Math.random() * (max-min) + min);
+}
+
+function fontStyle(fsize = 16, color = "#000", align = LEFT, style = NORMAL, lineheight = fsize*1.2, font = null){
+    fill(color);
+    textSize(fsize);
+    textAlign(align);
+    textStyle(style);
+    textLeading(lineheight);
+
+    if (font!= null){
+        textFont(font);
+    }
+}
+
+function flip(){
+    return rand (-1, 1) > 0; 
+}
