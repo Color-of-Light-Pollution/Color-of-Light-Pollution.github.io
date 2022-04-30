bc6667594b4cada317fd44a65d803498ce54fd23
acelaena <62402838+acelaena@users.noreply.github.com>
2022-04-30 03:08:38 -0700

    yeet

--- CLPproject/timelapse/sketch.js
+++ CLPproject/timelapse/sketch.js
@@ -1,6 +1,5 @@
 /**
-	Project 1 - Vale Pet Shelter
-	by Acelaena
+	By Acelaena
 
     Overview:
     A simple color-picker game that makes use of p5.clickable and p5.timer. 
@@ -14,141 +13,71 @@
     - i also modified p5.clickable and p5.timer. 
 */
 
-const BGCOL = '#e8f6ce';
-var PXSIZE = 100;   //default; is set to min(PXSIZE, SCREENSIZE/3);
-const WAIT = 2000;
-const PXWIDTH = 3;
-const PXHEIGHT = 2;
-var OFFX; //offsets for pixel grid
-var OFFY; 
+const BGCOL = '#2a2a2a';
+const WAIT = 1000;
+var data;
+var colorList;
+var colorct = 0;
+var TempList; 
 
 
 //game handlers
 var timer_wait;
-var randColor = [];
-var variColor = [];
-
-var rand_coord_x;
-var rand_coord_y; 
-var pixel_canvas; 
-
-var points = 0; 
-var lives = 5; 
 var newSlide = true;
-var gameOver = false;
 
 
-//controls
-var clicker; 
-var click1;
-var newSlide = true;
 
 /**
     Keyboard navigation support. 
 */
-function keyTyped() {
-    console.log("key typed: "+ key);
-}
- 
-/**
-    Mouse navigation support.
-*/
-function mousePressed() {
-    if (clicker.outsideClickable()){
-        lives--;
-        newSlide = true;
-        if (lives <= 0){ gameOver = true; }
-    }
-}
-
-//function outsideClickable(){
-//    return (mouseX < clicker.x || mouseX > clicker.x+PXSIZE) || (mouseY < clicker.y || mouseY > clicker.y+PXSIZE);
-//}
 
 function setup() {
     //remove scrollbar width because p5 smelly and I cant do my elegant no-scrollbar trick
-    createCanvas(windowWidth-12, windowHeight-12);
+    createCanvas(windowWidth, windowHeight-104);
     background(BGCOL);
     frameRate(20);
+    
     //constant setup
-    PXSIZE = min(PXSIZE, min(width, height)/3);
-    OFFX = width/2 - PXWIDTH*PXSIZE/2;
-    OFFY = height/2 - PXHEIGHT*PXSIZE/2;
     textSize(24);
+    textFont("Source Sans Pro");
+    data = document.getElementById("csv").innerHTML.trim().split("\n");
+    colorList = data.filter(element => {
+        return element !== '';
+    });
 
+    
     //timer setup
     timer_wait = new Timer(WAIT);
-    
-    //create canvas
-    pixel_canvas = new PixelCanvas(PXWIDTH, PXHEIGHT, PXSIZE, '#000', OFFX, OFFY);
-    pixel_canvas.setPixel(2,1, color(255, 255, 0));
-    
-    //clicker setup 
-    clicker = new Clickable(); 
-    clicker.color = '#0000'; 
-    clicker.text = '';
-    clicker.strokeWeight = 0;  
-    clicker.resize(PXSIZE,PXSIZE);
-    clicker.cornerRadius = 0;
-    clicker.onPress = function(){  //When myButton is pressed
-        newSlide = true;
-        points++;
-    }  
-//    clicker.onOutsidePress = function(){
-//        lives--;
-//        newSlide = true;
-//        if (lives <= 0){ gameOver = true; }
-//    }
 }
 
 function draw() {
-    fill('#000'); 
+    fill('#fff'); 
     strokeWeight(0);
-    textAlign(CENTER);
-    text("Click the color that looks different from the rest!", width/2, 50);
-    
-    if (gameOver){
-        background(BGCOL);
-        textAlign(CENTER);
-        text("GAME OVER D:\n Refresh to play again!", width/2, height/2);
-        newSlide = false;
-        noLoop();
-        textAlign(LEFT);
-    }
+    textAlign(LEFT);
     
     if (newSlide){
-        background(BGCOL);
-        for (var i = 0; i < 3; i++){
-            randColor[i] = rand(25, 230);
-            variColor[i] = rand(5, 25); 
-            if (flip()){ variColor[i] = -variColor[i]; }
-        }
-    
-        for(i = 0; i < PXHEIGHT; i++){ //rows
-            for (var j = 0; j < PXWIDTH; j++) { //columns
-                pixel_canvas.setPixel(j, i, color(randColor[0], randColor[1], randColor[2]));
-            }
-        }
+        //bg color loop
+        if (colorct == colorList.length){colorct = 0;}
+        background(colorList[colorct]);
         
-        rand_coord_x = rand(0, PXWIDTH-1);
-        rand_coord_y = rand(0, PXHEIGHT-1);
-        pixel_canvas.setPixel(rand_coord_x, rand_coord_y, color(randColor[0]+variColor[0], randColor[1]+variColor[1], randColor[2]+variColor[2]));
+        //AQI
+        textSize(24);
+        text("AQI", 60, windowHeight-330);
+        textSize(180);
+        text("44", 40, windowHeight-204);
         
-        clicker.locate(OFFX + rand_coord_x*PXSIZE, OFFY + rand_coord_y*PXSIZE);
+        //PM 
+        textSize(24);
+        text("Highest Pollutant:", 60, windowHeight-170);
+        
         timer_wait.reset();
-        
+        colorct++;
         newSlide = false; 
     }
-    fill('#000'); 
-    strokeWeight(0);
-    text("Points: " + points, width/2-300, 100);
-    text("Lives: " + lives, width/2+300, 100);
-    clicker.draw(); 
+
     
-//    console.log(timer_wait.getRemainingTime());
+    
     if (timer_wait.expired()){
-        lives--; 
         newSlide = true;
-        if (lives <= 0){ gameOver = true; }
     }
 }
